7c65be63e77e61376a7ece98e3cc99c2036cd0cc
Menaka-anvi <menakasubramanian12@gmail.com>
2026-05-24 14:35:03 +0530

    Removed duplicate image tag

--- theme.js
+++ theme.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-const themes = document.getElementsByClassName("theme");
-
-Array.from(themes).forEach((theme) => {
-  theme.addEventListener("click", (e) => {
-    document
-      .querySelector("body")
-      .setAttribute("data-theme", e.target.dataset.theme);
-    localStorage.setItem("theme", e.target.dataset.theme);
-  });
-});
-
-function getThemeOnLoad() {
-  const theme = localStorage.getItem("theme");
-
-  if (theme) {
-    document.querySelector("body").setAttribute("data-theme", theme);
-  }
-}
-
-getThemeOnLoad();
 
 const themes = document.getElementsByClassName("theme");
 
@@ -40,4 +19,4 @@ function getThemeOnLoad() {
 }
 
 getThemeOnLoad();
->>>>>>> c716629 (Fixed image path for profile picture)
+
