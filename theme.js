2eca172ef61a8d3f1b6d4a5bf045bca82b7a1bb8
Menaka-anvi <menakasubramanian12@gmail.com>
2026-05-24 14:00:42 +0530

    Fixed image path for profile picture

--- theme.js
+++ theme.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const themes = document.getElementsByClassName("theme");
 
 Array.from(themes).forEach((theme) => {
@@ -18,3 +19,25 @@ function getThemeOnLoad() {
 }
 
 getThemeOnLoad();
+
+const themes = document.getElementsByClassName("theme");
+
+Array.from(themes).forEach((theme) => {
+  theme.addEventListener("click", (e) => {
+    document
+      .querySelector("body")
+      .setAttribute("data-theme", e.target.dataset.theme);
+    localStorage.setItem("theme", e.target.dataset.theme);
+  });
+});
+
+function getThemeOnLoad() {
+  const theme = localStorage.getItem("theme");
+
+  if (theme) {
+    document.querySelector("body").setAttribute("data-theme", theme);
+  }
+}
+
+getThemeOnLoad();
+>>>>>>> c716629 (Fixed image path for profile picture)
